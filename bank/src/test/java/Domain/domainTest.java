cf51cc903fc8c4589d4fc133569b2de2b1570827
thijnofant <tavandijk@live.nl>
2015-11-17 14:47:48 +0100

    test1 inserted

--- bank/src/test/java/Domain/domainTest.java
+++ bank/src/test/java/Domain/domainTest.java
@@ -5,12 +5,17 @@
  */
 package Domain;
 
+import bank.domain.Account;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import util.DatabaseCleaner;
 
 /**
  *
@@ -18,28 +23,33 @@ import static org.junit.Assert.*;
  */
 public class domainTest {
     
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
+    EntityManager em = emf.createEntityManager();
+    DatabaseCleaner dc = new DatabaseCleaner(em);   
+    
     public domainTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
     }
     
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        try{
+            dc.clean();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void opdracht1() {
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        assertNull(account.getId());
+        em.getTransaction().commit();
+        System.out.println("AccountId: " + account.getId());
+        //TODO: verklaar en pas eventueel aan
+        assertTrue(account.getId() > 0L);
+    }
 }
